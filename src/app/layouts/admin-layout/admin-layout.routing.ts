52f18d8f97f22a7d8119b9342d82b50d903dd997
yahianiismail <Hawma@122020>
2023-11-25 20:34:09 +0100

    set login register components

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -11,13 +11,13 @@ import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {LoginComponent} from "../../login/login/login.component";
 
 export const AdminLayoutRoutes: Routes = [
-    { path: 'dashboard',      component: HomeComponent },
-    { path: 'user',           component: UserComponent },
-    { path: 'table',          component: TablesComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
+            { path: 'dashboard',      component: HomeComponent },
+            { path: 'user',           component: UserComponent },
+            { path: 'table',          component: TablesComponent },
+            { path: 'typography',     component: TypographyComponent },
+            { path: 'icons',          component: IconsComponent },
+            { path: 'maps',           component: MapsComponent },
+            { path: 'notifications',  component: NotificationsComponent },
+            { path: 'upgrade',        component: UpgradeComponent },
     { path: 'login',        component: LoginComponent }
 ];
