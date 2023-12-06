96c9f3625879fdba99f689c4adddad9c748c5d7b
yahianiismail <Hawma@122020>
2023-12-06 23:25:52 +0100

    set facebook  authent

--- src/app/login/login/facbook-authent/facbook-authent.component.ts
+++ src/app/login/login/facbook-authent/facbook-authent.component.ts
@@ -1,32 +1,50 @@
 import {Component, OnInit} from '@angular/core';
 import {FacebookLoginProvider, SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
-import {AccountService} from "../account.service";
+
 import {environment} from "../../../../environments/environment";
 import {finalize} from "rxjs/operators";
-declare const FB: any;
+import {logging} from "protractor";
+import {concatMap, EMPTY, from, of} from "rxjs";
+import {LoginService} from "../login.service";
+declare var FB: any;
 @Component({
   selector: 'app-facbook-authent',
   templateUrl: './facbook-authent.component.html',
   styleUrls: ['./facbook-authent.component.scss']
 })
+
 export class FacebookAuthentComponent implements OnInit{
   socialUser!: SocialUser;
   isLoggedin?: boolean = undefined;
-  constructor(
-      private accountService: AccountService,
-      private socialAuthService : SocialAuthService
 
-  ) {
-  }
-  loginWithFacebook(): void {
-    this.socialAuthService.authState.subscribe((user) => {
-      console.log(user);
-      this.socialUser = user;
-      this.isLoggedin = user != null;
-    });
-  }
+  constructor(private loginService : LoginService){}
 
   ngOnInit(): void {
+   (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: environment.facbookID,
+        cookie: true,
+        xfbml: true,
+        version: 'v18.0'
+      });
+
+    };
+
+    // load facebook sdk script
+    (function (d, s, id) {
+      var js: any;
+      var fjs: any = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
   }
+    login() {
+        // login with facebook then authenticate with the API to get a JWT auth token
+        this.loginService.login();
+    }
+
 }
